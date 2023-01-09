d5c3736836ac60a51537febc2d6725f1b23099ba
v3ry <v3ryfr@gmail.com>
2023-01-09 21:22:39 +0100

    first commit

--- src/00.intro.ts
+++ src/00.intro.ts
@@ -1,22 +1,27 @@
-import {Observable, of} from 'rxjs';
-import {filter, map} from 'rxjs/operators';
+import { Observable, of, range } from "rxjs";
+import { filter, map } from "rxjs/operators";
 
 /*
     DO NOT TOUCH CONST NAMES (Otherwise, tests won't work)
  */
 
 // Just an observable which should send "Hello World"
-export const helloWorldObservable = of('Hello');
+export const helloWorldObservable = of("Hello World");
 
 // This one must trigger numbers from 0 to 10
 // Pro tip: You can also use `range` from RxJS
-export const zeroToTenObservable = of([0]);
+export const zeroToTenObservable = range(0, 11);
+console.log(zeroToTenObservable);
 
 // Use map operator to pick the first letter au each word
-export const firstLetterObservable = of('Wild', 'Code', 'School').pipe();
+export const firstLetterObservable = of("Wild", "Code", "School").pipe(
+  map((val) => val[0])
+);
 
 // Use filter operator to allow only positive numbers
-export const positiveNumbersObservable = of(-23, 543, 7, 6, 3, -234, 43).pipe()
+export const positiveNumbersObservable = of(-23, 543, 7, 6, 3, -234, 43).pipe(
+  filter((x) => x >= 0)
+);
 
 // Write a function that produce the first 15th fibonnaci numbers
 // In mathematics, the Fibonacci numbers, commonly denoted Fn form a sequence,
@@ -31,5 +36,18 @@ export const positiveNumbersObservable = of(-23, 543, 7, 6, 3, -234, 43).pipe()
 //
 // NOTE: Do not use `scan` operator
 export const fibonacciObservable = new Observable(function (observer) {
+  let value = 0;
+  let lastValue = 0;
+  let list: Array<number> = [];
+  const interval = setInterval(() => {
+    value++;
+    // observer.next(value);
+    list.push(value);
+  });
 
+  if ((value = 15)) {
+    observer.next(list);
+    observer.complete();
+    clearInterval(interval);
+  }
 });
